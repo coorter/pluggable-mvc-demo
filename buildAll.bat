1d90696c779fd044e6ff7731de1929dd5fd90dc5
Lukasz Szczepanik <lukasz.szczepanik@outlook.com>
2016-11-08 22:05:55 +0100

    Compilation script

---
+++ buildAll.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" PluggableDemoRoot.sln
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Plugins\DemoPluginOne\DemoPluginOne.sln
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Plugins\DemoPluginTwo\DemoPluginTwo.sln
